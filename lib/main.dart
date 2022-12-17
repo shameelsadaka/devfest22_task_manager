a7b081e2f4b98cda0ca36a1f9441e1813ca83062
Aloysious Benoy <aloysiousbenoy@gmail.com>
2022-12-17 15:43:29 +0530

    Implemented add and update task.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sample_project/task_model.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,140 +30,172 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget{
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<TaskItemModel> tasks = [TaskItemModel("Hello ", "", DateTime.now(), false)];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey.shade200,
-      body: SizedBox.expand(
-        child: Stack(
-          children: [
-            Image.asset("assets/sky.jpeg"),
-            SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text("Task Manager", style:TextStyle(color: Colors.white),),
-                  SizedBox(
-                    height: 60,
-                  ),
-                  Text(
-                    "December 19",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 22,
-                      fontWeight: FontWeight.w600
-                    )
-                  ),
-                  SizedBox(height: 20),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Container(
-                      width: double.infinity,
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15)
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          TaskItem(),
-                          TaskItem(),
-                          TaskItem(),
-                          TaskItem(),
-                          TaskItem(),
-                          TaskItem(),
-                          TaskItem(),
-                          TaskItem(),
-                          TaskItem(),
-                          TaskItem(),
-                        ],
-                      ),
+        backgroundColor: Colors.blueGrey.shade200,
+        body: SizedBox.expand(
+          child: Stack(
+            children: [
+              Image.asset("assets/sky.jpeg"),
+              SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 15.0),
-                        child: Text("Completed"),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: Colors.white
-                      ),
-                      child: Column(
-                        children: [
-                          TaskItem(),
-                          TaskItem(),
-                          TaskItem(),
-                        ],
-                      ),
+                    Text(
+                      "Task Manager",
+                      style: TextStyle(color: Colors.white),
                     ),
-                  ),
-            
-                  SizedBox(height: 30),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 15.0, right: 15),
-                    child: SizedBox(
-                      width: double.infinity,
-                      height: 40,
-                      child: TextButton(
-                        onPressed: (){},
-                        child: Text("Add Task"),
-                        style: TextButton.styleFrom(
-                          backgroundColor: Colors.blue.shade700,
-                          foregroundColor: Colors.white
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Text("December 19",
+                        style: TextStyle(
+                            color: Colors.white, fontSize: 22, fontWeight: FontWeight.w600)),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 56, 39, 39),
+                            borderRadius: BorderRadius.circular(15)),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: tasks
+                              .map((e) => TaskItem(
+                                    task: e,
+                                    onChange: (bool value) {
+                                      setState(() {
+                                        tasks
+                                            .firstWhere((elm) => elm.title == e.title)
+                                            .isCompleted = value;
+                                      });
+                                    },
+                                  ))
+                              .toList()
+                              .where((element) => !element.task.isCompleted)
+                              .toList(),
                         ),
                       ),
                     ),
-                  )
-                ],
+                    SizedBox(height: 20),
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 15.0),
+                          child: Text("Completed"),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(15), color: Colors.white),
+                        child: Column(
+                          children: tasks
+                              .map((e) => TaskItem(
+                                    task: e,
+                                    onChange: (bool value) {
+                                      setState(() {
+                                        tasks
+                                            .firstWhere((elm) => elm.title == e.title)
+                                            .isCompleted = value;
+                                      });
+                                    },
+                                  ))
+                              .toList()
+                              .where((element) => element.task.isCompleted)
+                              .toList(),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 15.0, right: 15),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 40,
+                        child: TextButton(
+                          onPressed: () {
+                            setState(() {
+                              tasks.add(TaskItemModel("new task", "", DateTime.now(), false));
+                            });
+                          },
+                          child: Text("Add Task"),
+                          style: TextButton.styleFrom(
+                              backgroundColor: Colors.blue.shade700, foregroundColor: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ),
-            ),
-          ],
-        ),
-      )
-    );
+            ],
+          ),
+        ));
   }
 }
 
-class TaskItem extends StatelessWidget {
+class TaskItem extends StatefulWidget {
+  final TaskItemModel task;
+
+  final void Function(bool value) onChange;
+
   const TaskItem({
     Key? key,
+    required this.task,
+    required this.onChange,
   }) : super(key: key);
+
+  @override
+  State<TaskItem> createState() => _TaskItemState();
+}
+
+class _TaskItemState extends State<TaskItem> {
+  bool isChecked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isChecked = widget.task.isCompleted;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 5, bottom: 5),
+      padding: EdgeInsets.only(top: 5, bottom: 5),
       child: Row(
         children: [
           CircleAvatar(
             radius: 20,
             backgroundColor: Colors.green,
-            child: Icon(
-              Icons.place
-            ),
+            child: Icon(Icons.place),
           ),
           SizedBox(width: 10),
-          Text("Task 1"),
+          Text(
+              "${widget.task.title}  ${widget.task.timestamp.hour}:${widget.task.timestamp.minute}"),
           Spacer(),
           Checkbox(
-            value: false,
-            onChanged: null
-          )
+              value: widget.task.isCompleted,
+              onChanged: (bool? value) {
+                widget.onChange(value ?? false);
+              })
         ],
       ),
     );
---
+++ lib/task_model.dart
@@ -0,0 +1,8 @@
+class TaskItemModel {
+  final String title;
+  final String desc;
+  final DateTime timestamp;
+  bool isCompleted;
+
+  TaskItemModel(this.title, this.desc, this.timestamp, this.isCompleted);
+}
