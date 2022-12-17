dc7abc883f13705af024d2182f7ed0484c9f28cb
Shameel Kadannamanna <shameelsadaka@gmail.com>
2022-12-17 12:42:50 +0530

    First UI

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade100,
+      body: SizedBox.expand(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("Task Manager"),
+            SizedBox(
+              height: 100,
+            ),
+            Text(
+              "December 19",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600
+              )
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.green,
+                          child: Icon(
+                            Icons.place
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text("Task 1"),
+                        Spacer(),
+                        Checkbox(
+                          value: false,
+                          onChanged: null
+                        )
+                      ],
+                    ),
+                    Text("Task 2")
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+}
